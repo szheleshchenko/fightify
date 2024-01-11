ed18b469a533b2edf5b4c2596011d6cd2fdc6b6e
Sergei Zheleshchenko <szheleshchenko@ronasit.com>
2024-01-11 22:31:01 +0600

    feat(FeaturedVideo): implement layout

--- src/libs/shared/features/home-overview/home-overview.component.ts
+++ src/libs/shared/features/home-overview/home-overview.component.ts
@@ -1,13 +1,14 @@
 import {Component} from '@angular/core';
 import {EventCardComponent} from '@libs/event/features/event-card';
 import {HomeNewsSectionComponent} from '@libs/news/features/home-news-section';
+import {VideoCardComponent} from '@libs/news/features/video-card';
 import {Event} from '@libs/shared/data-access/api/event';
 import {ContentCardComponent} from '@libs/shared/ui/ui-kit/content-card';
 
 @Component({
   selector: 'app-home-overview',
   standalone: true,
-  imports: [ContentCardComponent, HomeNewsSectionComponent, EventCardComponent],
+  imports: [ContentCardComponent, HomeNewsSectionComponent, EventCardComponent, VideoCardComponent],
   templateUrl: './home-overview.component.html',
   styleUrl: './home-overview.component.scss',
 })
