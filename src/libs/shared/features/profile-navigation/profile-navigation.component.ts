57f529e0ec02689b1d191fe26bca103c6fd93354
Sergei Zheleshchenko <szheleshchenko@ronasit.com>
2024-02-05 00:37:47 +0600

    feat(DropdownOption): update layout

--- src/libs/shared/features/profile-navigation/profile-navigation.component.ts
+++ src/libs/shared/features/profile-navigation/profile-navigation.component.ts
@@ -1,14 +1,24 @@
 import {Component} from '@angular/core';
 import {DropdownComponent} from '@libs/shared/ui/ui-kit/dropdown';
-import {DropdownOption} from '@libs/shared/ui/ui-kit/dropdown-option';
+import {DropdownOption, DropdownOptionComponent} from '@libs/shared/ui/ui-kit/dropdown-option';
 
 @Component({
   selector: 'app-profile-navigation',
   standalone: true,
-  imports: [DropdownComponent],
+  imports: [DropdownComponent, DropdownOptionComponent],
   templateUrl: './profile-navigation.component.html',
   styleUrl: './profile-navigation.component.scss',
 })
 export class ProfileNavigationComponent {
-  public links: Array<DropdownOption> = [];
+  public links: Array<DropdownOption> = [
+    {
+      title: 'Settings',
+    },
+    {
+      title: 'Bets',
+    },
+    {
+      title: 'Comments history',
+    },
+  ];
 }
