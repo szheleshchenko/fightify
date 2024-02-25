0df6f0da6ba50ab46727b74804101ff31a848c7d
Sergei Zheleshchenko <szheleshchenko@ronasit.com>
2024-02-25 23:52:36 +0600

    feat(LoginFormPage): update layout

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -1,10 +1,19 @@
 import {Component} from '@angular/core';
+import {Breadcrumb, PageComponent} from '@libs/shared/features/page';
+import {RegisterFormComponent} from '@libs/user/features/register-form';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [PageComponent, RegisterFormComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  public breadcrumbs: Array<Breadcrumb> = [
+    {
+      translationKey: 'SHARED.NAVIGATION.TEXT_LOGIN',
+      path: '/login',
+    },
+  ];
+}
--- src/libs/user/ui/login-form-wrapper/login-form-wrapper.component.ts
+++ src/libs/user/ui/login-form-wrapper/login-form-wrapper.component.ts
@@ -1,13 +1,15 @@
 import {Component, Input} from '@angular/core';
+import {ButtonComponent} from '@libs/shared/ui/ui-kit';
 
 @Component({
   selector: 'app-login-form-wrapper',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './login-form-wrapper.component.html',
   styleUrl: './login-form-wrapper.component.scss',
 })
 export class LoginFormWrapperComponent {
+  @Input() buttonText!: string;
   @Input() heading!: string;
   @Input() subheading?: string;
 }
