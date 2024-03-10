48495bbdf217a9cdb919485c85ff22927fca3e86
Sergei Zheleshchenko <szheleshchenko@ronasit.com>
2024-03-10 20:49:16 +0600

    feat(EventPage): update layout

---
+++ src/libs/event/ui/events-status-filter/index.ts
@@ -0,0 +1,1 @@
+export * from './events-status-filter.component';
--- src/libs/shared/ui/ui-kit/select/select.component.ts
+++ src/libs/shared/ui/ui-kit/select/select.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input} from '@angular/core';
-import {SelectOption} from './models';
+import {Component, ElementRef, HostListener, Input, ViewChild, forwardRef} from '@angular/core';
+import {NG_VALUE_ACCESSOR} from '@angular/forms';
 import {BaseFormControlDirective} from '@libs/shared/utils/form';
+import {SelectOption} from './models';
 
 @Component({
   selector: 'app-select',
@@ -8,8 +9,35 @@ import {BaseFormControlDirective} from '@libs/shared/utils/form';
   imports: [],
   templateUrl: './select.component.html',
   styleUrl: './select.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true,
+    },
+  ],
 })
-export class SelectComponent<T> extends BaseFormControlDirective<T> {
+export class SelectComponent<T> extends BaseFormControlDirective<SelectOption<T>> {
+  @ViewChild('select') select!: ElementRef<HTMLDivElement>;
+  @ViewChild('input') input!: ElementRef<HTMLInputElement>;
+
   @Input() options: Array<SelectOption<T>> = [];
   @Input() label!: string;
+
+  @HostListener('document:mousedown', ['$event'])
+  public onOutsideClick(event: MouseEvent): void {
+    if (!this.select.nativeElement.contains(event.target as Node)) {
+      this.disableTrigger();
+    }
+  }
+
+  public disableTrigger(): void {
+    this.input.nativeElement.checked = false;
+  }
+
+  public changed(option: SelectOption<T>): void {
+    this.disableTrigger();
+    this.valueChanged(option);
+    this.writeValue(option);
+  }
 }
