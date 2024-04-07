0e0b99cb1168aabe662d57cf3e70c1dec5aef6a3
Sergei Zheleshchenko <szheleshchenko@ronasit.com>
2024-04-07 20:24:52 +0600

    feat(upcoming-events): update layout

---
+++ src/libs/event/features/upcoming-events/components/controls/upcoming-events-controls.component.ts
@@ -0,0 +1,13 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-upcoming-events-controls',
+  standalone: true,
+  imports: [],
+  templateUrl: './upcoming-events-controls.component.html',
+  styleUrl: './upcoming-events-controls.component.scss',
+})
+export class UpcomingEventsControlsComponent {
+  @Output() backClicked = new EventEmitter<void>();
+  @Output() nextClicked = new EventEmitter<void>();
+}
--- src/libs/event/features/upcoming-events/upcoming-events.component.ts
+++ src/libs/event/features/upcoming-events/upcoming-events.component.ts
@@ -1,18 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {OrderFilter} from '@libs/shared/ui/order-filter';
 import {SelectComponent, SelectOption} from '@libs/shared/ui/ui-kit';
 import {UpcomingEventComponent} from './components/upcoming-event';
+import {UpcomingEventsControlsComponent} from './components/controls';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-upcoming-events',
   standalone: true,
-  imports: [SelectComponent, ReactiveFormsModule, UpcomingEventComponent],
+  imports: [
+    SelectComponent,
+    ReactiveFormsModule,
+    UpcomingEventComponent,
+    UpcomingEventsControlsComponent,
+    NgStyle,
+  ],
   templateUrl: './upcoming-events.component.html',
   styleUrl: './upcoming-events.component.scss',
 })
 export class UpcomingEventsComponent {
   public form = new FormControl<SelectOption<OrderFilter>>({title: 'Ascending', value: 'asc'});
+
+  public offset = signal(0);
 
   public options: Array<SelectOption<OrderFilter>> = [
     {
@@ -24,4 +34,14 @@ export class UpcomingEventsComponent {
       value: 'desc',
     },
   ];
+
+  public slideChanged(direction: 1 | -1): void {
+    this.offset.update((offset) => {
+      if (direction === 1) {
+        return offset >= 0 ? offset : offset + direction * 293;
+      }
+
+      return offset + direction * 293;
+    });
+  }
 }
