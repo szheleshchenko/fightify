8ebb6c23ff4954fcd1db3dcf7893f93e8fc44087
Sergei Zheleshchenko <szheleshchenko@ronasit.com>
2024-10-04 20:58:26 +0300

    feat(HomeOverview): add translations

--- src/libs/shared/features/home-overview/components/most-popular-posts/most-popular-posts.component.ts
+++ src/libs/shared/features/home-overview/components/most-popular-posts/most-popular-posts.component.ts
@@ -1,10 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TranslocoDirective} from '@jsverse/transloco';
 import {NewsItemComponent} from '@libs/news/features/news-item';
 
 @Component({
   selector: 'app-home-overview-most-popular-posts',
   standalone: true,
-  imports: [NewsItemComponent],
+  imports: [NewsItemComponent, TranslocoDirective],
   templateUrl: './most-popular-posts.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
