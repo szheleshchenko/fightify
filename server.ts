c576484500e0ebbd675b99e5291bd29bc68ac4ed
Sergei Zheleshchenko <szheleshchenko@ronasit.com>
2025-01-02 14:38:13 +0300

    feat: update ssr implementation

--- server.ts
+++ server.ts
@@ -1,63 +1,46 @@
-import {APP_BASE_HREF} from '@angular/common';
-import {CommonEngine} from '@angular/ssr/node';
+import {
+  AngularNodeAppEngine,
+  createNodeRequestHandler,
+  isMainModule,
+  writeResponseToNodeResponse,
+} from '@angular/ssr/node';
 import express from 'express';
-import {REQUEST as SSR_REQUEST} from 'ngx-cookie-service-ssr';
-import {dirname, join, resolve} from 'node:path';
+import {dirname, resolve} from 'node:path';
 import {fileURLToPath} from 'node:url';
-import bootstrap from './src/main.server';
 
-// The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
+  const angularNodeAppEngine = new AngularNodeAppEngine();
 
-  const commonEngine = new CommonEngine();
-
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
-
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
   server.get(
-    '*.*',
+    '**',
     express.static(browserDistFolder, {
       maxAge: '1y',
+      index: 'index.html',
     }),
   );
 
-  // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const {protocol, originalUrl, baseUrl, headers} = req;
-
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [
-          {provide: APP_BASE_HREF, useValue: baseUrl},
-          {provide: SSR_REQUEST, useValue: req},
-        ],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+  server.get('**', (req, res, next) => {
+    angularNodeAppEngine
+      .handle(req, {server: 'express'})
+      .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
+      .catch(next);
   });
 
   return server;
 }
 
-function run(): void {
-  const port = process.env['PORT'] || 4000;
+const server = app();
 
-  // Start up the Node server
-  const server = app();
+if (isMainModule(import.meta.url)) {
+  const port = process.env['PORT'] || 4000;
   server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+    console.log(`Node Express server listening on http://localhost:\${port}`);
   });
 }
 
-run();
+console.warn('Node Express server started');
+
+export const reqHandler = createNodeRequestHandler(server);
